9b0ec8d1a433bde06cf89dd44bd3d3795d147fd7
luko <simonenkorostik@gmail.com>
2024-10-03 19:51:54 +0300

    1

--- src/sections/SectionPortfolio.jsx
+++ src/sections/SectionPortfolio.jsx
@@ -21,17 +21,14 @@ function SectionPortfolio() {
             <ContentCard id="1" className='card contentcard-promo' href={links['Promo2.0-yt']} />
             <ContentCard id="2" className='card contentcard-promo' href={links['Promo2.1-yt']} />
             <ContentCard id="3" className='card contentcard-promo' href={links['Promo2.2-yt']} />
-
-      </div>
-      <div className='h1' style={{fontSize: '60px', textAlign:'center'}}>
+          </div>
+      {/* <div className='h1' style={{fontSize: '60px', textAlign:'center'}}>
       {t('PortfolioTitleLooped')}
       </div>
           <div className="card-container" style={{marginBottom: '2rem'}}>
             <ContentCard id="4" className='card contentcard-loop' noButton/>
             <ContentCard id="5" className='card contentcard-loop' />
-            {/* <ContentCard id="6" className='contentcard-loop' /> */}
-      </div>
-      
+          </div> */}
     </div>
   );
 }
